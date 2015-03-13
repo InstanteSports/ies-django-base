42d45d1cffbbeaaf230c312e0ac8fae2a06437f5
jonzlin95 <jonzlin95@gmail.com>
2015-03-13 01:43:13 -0700

    recreated the package

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='ies-django-base',
     version='0.1',
-    packages=['ies_base'],
+    packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
     description='Base apps for Instant eSports django use.',
