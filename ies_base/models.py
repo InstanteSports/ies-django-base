a9a883c4421e312b83e6014336d457cd9d6c2185
jonzlin95 <jonzlin95@gmail.com>
2016-12-02 14:04:31 -0800

    One more autofield

--- ies_base/models.py
+++ ies_base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.db.models.fields.related import ManyToOneRel, AutoField, ManyToManyField, RelatedField
+from django.db.models.fields.related import ManyToOneRel, ManyToManyField, RelatedField
+from django.db.models.fields import AutoField
 from django import VERSION
 
 
